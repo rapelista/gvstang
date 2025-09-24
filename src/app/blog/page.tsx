af3ebb7824da89a724b8cc31987af7b591901539
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-09-24 09:53:22 +0700

    feat: update Blog page to display list of markdown files as links

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,13 +3,17 @@ import type { Metadata } from 'next';
 import { readdirSync } from 'node:fs';
 import path from 'node:path';
 
+import Link from 'next/link';
+
 export const metadata: Metadata = {
   title: 'Blog',
 };
 
 export default function Page() {
   const blogPostsDir = path.join(process.cwd(), 'src/markdown/blog');
-  const files = readdirSync(blogPostsDir);
+  const files = readdirSync(blogPostsDir).map((file) =>
+    file.replace(/\.md?$/, ''),
+  );
 
   return (
     <main className="md:col-span-2 space-y-6">
@@ -17,7 +21,15 @@ export default function Page() {
 
       <hr className="opacity-10" />
 
-      <pre>{JSON.stringify(files, null, 2)}</pre>
+      <ul>
+        {files.map((file, key) => (
+          <li key={key}>
+            <Link className="capitalize" href={`/blog/${file}`}>
+              {file}
+            </Link>
+          </li>
+        ))}
+      </ul>
 
       <hr className="opacity-10" />
 
