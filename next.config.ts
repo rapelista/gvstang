846f606d1d33bf33e16c8b3ef5ea03cd600ae7a3
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-09-24 10:37:17 +0700

    feat: add remark-gfm plugin for enhanced markdown support; create GFM example markdown file

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
 const withMdx = createMdx({
   extension: /\.(md|mdx)$/,
   options: {
-    remarkPlugins: ['remark-frontmatter'],
+    remarkPlugins: ['remark-frontmatter', 'remark-gfm'],
   },
 });
 
