62e2675acd09ff6b95925269c0c328b23d6aa322
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-09-24 11:42:56 +0700

    fix: adjust class order in Layout component for consistent styling

--- src/app/blog/[slug]/layout.tsx
+++ src/app/blog/[slug]/layout.tsx
@@ -4,9 +4,10 @@ export default function Layout({ children }: React.PropsWithChildren) {
   return (
     <div
       className={cn(
+        'max-w-screen',
         'prose dark:prose-invert prose-headings:mt-8 prose-headings:font-semibold prose-headings:text-black prose-h1:text-5xl prose-h2:text-4xl prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg dark:prose-headings:text-white',
-        'md:col-span-2',
         'prose-h1:first-of-type:mt-0',
+        'md:col-span-2',
       )}
     >
       {children}
