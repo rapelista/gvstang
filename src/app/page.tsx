fabbe5098838a8a2fbe0be5ce3f746719382d93c
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-09-24 09:35:04 +0700

    feat: add Tailwind CSS configuration and enhance styling across components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Page() {
       {Array.from({ length: 3 }, (_, i) => (
         <div
           key={i}
-          className="h-40 aspect-video bg-white/10 animate-pulse"
+          className="h-40 aspect-video bg-accent animate-pulse"
         ></div>
       ))}
 
@@ -33,7 +33,7 @@ export default function Page() {
       {Array.from({ length: 3 }, (_, i) => (
         <div
           key={i}
-          className="h-40 aspect-video bg-white/10 animate-pulse"
+          className="h-40 aspect-video bg-accent animate-pulse"
         ></div>
       ))}
     </main>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
       {Array.from({ length: 6 }, (_, i) => (
         <div
           key={i}
-          className="h-40 aspect-video bg-white/10 animate-pulse"
+          className="h-40 aspect-video bg-accent animate-pulse"
         ></div>
       ))}
     </main>
