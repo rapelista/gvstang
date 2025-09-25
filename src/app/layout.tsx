f5e77cc9c3face7e49c67326370573ae413e4368
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-09-25 12:05:56 +0700

    feat: add GitHub and LinkedIn icons to the layout and enhance global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
+import Link from 'next/link';
 import { Suspense } from 'react';
 
+import { GitHub } from '~/assets/svg/github';
+import { LinkedIn } from '~/assets/svg/linked-in';
 import { BrandLoading } from '~/components/brand-loading';
 import { BrandWrapper } from '~/components/brand-wrapper';
 import { Navbar } from '~/components/navbar';
@@ -18,8 +21,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
-      <body className={cn(fontSans.variable, fontMono.variable, 'dark')}>
+    <html className="dark" lang="en" suppressHydrationWarning={true}>
+      <body className={cn(fontSans.variable, fontMono.variable)}>
         <div className="container mx-auto max-w-[1440px] min-h-[calc(100svh-3.25rem)] md:min-h-[calc(100svh-4.25rem)]">
           <div
             className={cn(
@@ -34,6 +37,22 @@ export default function RootLayout({
               </Suspense>
 
               <Navbar />
+
+              <ul className="flex gap-2">
+                <li>
+                  <Link href="https://github.com/rapelista" target="_blank">
+                    <GitHub height={16} width={16} />
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://www.linkedin.com/in/farihakmal"
+                    target="_blank"
+                  >
+                    <LinkedIn height={16} width={16} />
+                  </Link>
+                </li>
+              </ul>
             </div>
 
             {children}
