992aa068e15f86ce7e125e3893f38b807877fffa
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-09-28 15:45:37 +0700

    refactor: enhance ToCWrapper component with click outside detection for improved usability

--- src/components/toc/wrapper.tsx
+++ src/components/toc/wrapper.tsx
@@ -1,18 +1,41 @@
 'use client';
 
 import { Menu } from 'lucide-react';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 import { Button } from '~/components/ui/button';
 import { cn } from '~/lib/utils';
 
 export function ToCWrapper({ children }: React.PropsWithChildren) {
-  const ref = useRef<HTMLDivElement>(null);
+  const tocRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (!tocRef.current) return;
+      if (!buttonRef.current) return;
+
+      if (tocRef.current.contains(event.target as Node)) return;
+      if (buttonRef.current.contains(event.target as Node)) return;
+
+      const opened = tocRef.current.getAttribute('data-hidden') === 'false';
+
+      if (opened) {
+        tocRef.current.setAttribute('data-hidden', 'true');
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
 
   return (
     <>
       <div
-        ref={ref}
+        ref={tocRef}
         className={cn(
           'sticky top-8 max-h-[calc(100svh-64px)] space-y-2 overflow-hidden',
           'max-md:fixed max-md:bottom-24 max-md:right-6 max-md:backdrop-blur-xl max-md:pt-6 max-md:border max-md:rounded-xl max-md:max-w-[calc(100svw-48px)] max-md:data-[hidden=true]:opacity-0 max-md:data-[hidden=true]:pointer-events-none max-md:transition-opacity max-md:duration-200',
@@ -24,6 +47,7 @@ export function ToCWrapper({ children }: React.PropsWithChildren) {
       </div>
 
       <Button
+        ref={buttonRef}
         className={cn(
           'md:hidden',
           'fixed bottom-6 right-6 h-12 w-12 rounded-full',
@@ -31,13 +55,14 @@ export function ToCWrapper({ children }: React.PropsWithChildren) {
         size="icon"
         variant="secondary"
         onClick={() => {
-          if (ref.current) {
-            const isHidden = ref.current.getAttribute('data-hidden') === 'true';
+          if (tocRef.current) {
+            const isHidden =
+              tocRef.current.getAttribute('data-hidden') === 'true';
 
             if (isHidden) {
-              ref.current.setAttribute('data-hidden', 'false');
+              tocRef.current.setAttribute('data-hidden', 'false');
             } else {
-              ref.current.setAttribute('data-hidden', 'true');
+              tocRef.current.setAttribute('data-hidden', 'true');
             }
           }
         }}
